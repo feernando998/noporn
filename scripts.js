40d1e2772baae3c18c64b0b0581b2e0f1c870110
feernando998 <lucasfernando0970@gmail.com>
2022-12-07 17:36:43 -0300

    criação do formulario contato e melhoria das tags

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
-document.querySelector('#mode').addEventListener("click", (e) => {
+/*document.querySelector('#mode').addEventListener("click", (e) => {
     document.querySelector('body').classList.toggle('dark')
-})
+})*/
+
+function validarContato(){
+    var nome = document.forms["formContato"]["name"].value
+
+    if (nome == ""){
+        alert("Digite seu nome!")
+        return false
+    }else{
+        alert("Mensagem enviada com sucesso. Retornarei assim que possível se necessário! Obrigado, " + nome + ".")
+    }
+
+}
